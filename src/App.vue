a19cffd7f876d81677ddcf9435ae18824e225055
niuniu <610494004@qq.com>
2017-07-28 09:48:12 +0800

    'new'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  #app
+    color:$color-theme
+</style>
